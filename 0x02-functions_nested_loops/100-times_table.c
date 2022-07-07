5228a5c5ab9a262a0c7972992736d3008343d9e1
Monagane <monagane@gmail.com>
2022-07-06 21:12:37 -0700

    100table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,50 @@
 #include "main.h"
-/**
- * times_table -prints tables
- *
- * Return: Always 0.
- */
-void times_table(void)
-{
-	int a;
-	int b;
-	int c;
 
-	for (a = 0; a <= 9; a++)
+/**
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
+ */
+
+void print_times_table(int n)
+{
+	int a = 0, rep, b;
+
+	if (n < 0 || n > 15)
+		return;
+	while (a <= n)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b <= n; b++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
 			}
 			else
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
+		a++;
 	}
 }
+
