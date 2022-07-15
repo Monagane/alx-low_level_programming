1ad29336d004c21fba52aeaad3c1de7c5c70761e
Monagane <monagane@gmail.com>
2022-07-14 20:52:55 -0700

    point

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,6 @@ void print_to_98(int n);
 void print_times_table(int n);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
