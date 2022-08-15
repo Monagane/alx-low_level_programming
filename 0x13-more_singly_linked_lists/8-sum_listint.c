1ae7e35564c84dcd2cb51f0a3ba0cd2d1e800d6a
Monagane <monagane@gmail.com>
2022-08-15 10:21:25 -0700

    0x13

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,9 +2,9 @@
 
 /**
  *  sum_listint - sums up all the data in a linked list
- *   @head: head of the list
- *    
- *     Return: sum of the number
+ *  @head: head of the list
+ *
+ * Return: sum of the number
  */
 
 int sum_listint(listint_t *head)
